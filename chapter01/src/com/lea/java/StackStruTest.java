31be167e57c7a3b4791695635ed1fbc0ffcc566c
lzc <380639608@qq.com>
2021-03-01 23:01:25 +0800

    Initial commit

---
+++ chapter01/src/com/lea/java/StackStruTest.java
@@ -0,0 +1,14 @@
+package com.lea.java;
+
+/**
+ * @author lzc
+ */
+public class StackStruTest {
+
+    public static void main(String[] args) {
+//        int i = 2 + 3;
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
+}
